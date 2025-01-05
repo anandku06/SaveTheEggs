ed415e460c2871a1a3b5a372bd84ce26e4ad4ff9
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-01-06 00:58:17 +0530

    add draw and render methods to Player and Game classes for improved game rendering

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ window.addEventListener("load", function () {
     constructor(game) {
       // game args so that the player knows the limitations of the width and height for easier movement
       this.game = game;
+      this.x
+      this.y
+    }
+
+    draw(context){
+        context.beginPath() // to start a new shape and close any previous shape if any
+        context.arc(400, 500, 50, 0, Math.PI * 2) // used to draw a circle; expects 5 args; x,y coords for center point, radius, start angle in radians, end angle
+        context.fill()
     }
   } // movements and other things of the player
 
@@ -18,9 +26,13 @@ window.addEventListener("load", function () {
       this.height = this.canvas.height;
       this.player = new Player(this);
     }
+    render(context){ // draw or update all objects
+        this.player.draw(context)
+    }
   } // controls our whole logic of the game
 
   const game = new Game(canvas)
+  game.render(ctx)
   console.log(game);
 
   function animate() {} // animations of the game required
