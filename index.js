b90d5f21d2d1d138a4b878805214457fd116a86d
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-01-05 23:49:41 +0530

    add Player and Game classes to manage game logic and player movement

--- index.js
+++ index.js
@@ -3,4 +3,23 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d')
     canvas.width = 1280
     canvas.height = 720
+
+    class Player{
+        constructor(game){ // game args so that the player knows the limitations of the width and height for easier movement
+            this.game = game
+        }
+    } // movements and other things of the player 
+
+    class Game{
+        constructor(canvas){
+            this.canvas = canvas
+            this.width = this.canvas.width
+            this.height = this.canvas.height
+            this.player = new Player(this)
+        }
+    } // controls our whole logic of the game 
+
+    function animate(){
+
+    } // animations of the game required
 })
