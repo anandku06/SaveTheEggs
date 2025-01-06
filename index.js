2c9e5abdfc67158c7352e25c649a4b827ded3fe9
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-01-07 00:08:32 +0530

    refactor Player class to centralize position and improve drawing logic

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 1280;
   canvas.height = 720;
+  // these hard-coated values are executed here only once to minimise the frequent change of the color of the shape when multiple objects are made
+  ctx.fillStyle = 'white' // hard-coated the color of the 'fill' method with white
+  ctx.lineWidth = 3
+  ctx.strokeStyle = 'white'
 
   class Player {
     constructor(game) {
       // game args so that the player knows the limitations of the width and height for easier movement
       this.game = game;
-      this.x
-      this.y
+      // starting position of the player should be on the middle
+      this.collisionX = this.game.width * 0.5 // to keep them in the middle of the canvas
+      this.collisionY = this.game.height * 0.5
+      this.collisionRadius = 30
     }
 
     draw(context){
         context.beginPath() // to start a new shape and close any previous shape if any
-        context.arc(400, 500, 50, 0, Math.PI * 2) // used to draw a circle; expects 5 args; x,y coords for center point, radius, start angle in radians, end angle
-        context.fill()
+        context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2) // used to draw a circle; expects 5 args; x,y coords for center point, radius, start angle in radians, end angle
+        // context.fill() // default color is black ; used to fill color in the shape made
+        context.stroke() // used to outline the shape made ; default is black, 1px stroke
     }
   } // movements and other things of the player
 
