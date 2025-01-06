ae4251405e8965f301feae126d13126e53f34384
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-01-07 00:19:30 +0530

    enhance Player draw method to support semi-transparency and update canvas usage documentation in notes.md

--- index.js
+++ index.js
@@ -4,25 +4,34 @@ window.addEventListener("load", function () {
   canvas.width = 1280;
   canvas.height = 720;
   // these hard-coated values are executed here only once to minimise the frequent change of the color of the shape when multiple objects are made
-  ctx.fillStyle = 'white' // hard-coated the color of the 'fill' method with white
-  ctx.lineWidth = 3
-  ctx.strokeStyle = 'white'
+  ctx.fillStyle = "white"; // hard-coated the color of the 'fill' method with white
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "white";
 
   class Player {
     constructor(game) {
       // game args so that the player knows the limitations of the width and height for easier movement
       this.game = game;
       // starting position of the player should be on the middle
-      this.collisionX = this.game.width * 0.5 // to keep them in the middle of the canvas
-      this.collisionY = this.game.height * 0.5
-      this.collisionRadius = 30
+      this.collisionX = this.game.width * 0.5; // to keep them in the middle of the canvas
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 30;
     }
 
-    draw(context){
-        context.beginPath() // to start a new shape and close any previous shape if any
-        context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2) // used to draw a circle; expects 5 args; x,y coords for center point, radius, start angle in radians, end angle
-        context.fill() // default color is black ; used to fill color in the shape made
-        context.stroke() // used to outline the shape made ; default is black, 1px stroke
+    draw(context) {
+      context.beginPath(); // to start a new shape and close any previous shape if any
+      context.arc(
+        this.collisionX,
+        this.collisionY,
+        this.collisionRadius,
+        0,
+        Math.PI * 2
+      ); // used to draw a circle; expects 5 args; x,y coords for center point, radius, start angle in radians, end angle
+      context.save();
+      context.globalAlpha = 0.5; // sets the opacity of the shape to semi-transparent
+      context.fill(); // default color is black ; used to fill color in the shape made
+      context.restore();
+      context.stroke(); // used to outline the shape made ; default is black, 1px stroke
     }
   } // movements and other things of the player
 
@@ -33,13 +42,14 @@ window.addEventListener("load", function () {
       this.height = this.canvas.height;
       this.player = new Player(this);
     }
-    render(context){ // draw or update all objects
-        this.player.draw(context)
+    render(context) {
+      // draw or update all objects
+      this.player.draw(context);
     }
   } // controls our whole logic of the game
 
-  const game = new Game(canvas)
-  game.render(ctx)
+  const game = new Game(canvas);
+  game.render(ctx);
   console.log(game);
 
   function animate() {} // animations of the game required
--- notes.md
+++ notes.md
@@ -1,30 +1,32 @@
 ## Some JS concepts learned here
+
 - JS classes are hoisted, but they are not initialised until that particular line is read
 - Objects in JS are so called reference datatypes i.e. refers to the space where the main class is stored.
 
-
-
 # <canvas>
+
 - used to draw graphics, animations, and render game scenes directly on a webpage using JS.
 - essentially a container for graphics that you can draw using scripting
 
-    ## How to use <canvas>
-    - Get the context via JS
-        ```Javascript
-        const canvas = document.getElementById('myCanvas');
-        const ctx = canvas.getContext('2d'); // 2d param for 2d graphics rendering
-        ```
-        ### getContext() method in JS
-        - used with <canvas> element to obtain a drawing context, which is an object that provides methods and properties for rendering graphics on the canvas.
-        - Syntax:
-            ```Javascript
-            canvas.getContext(contextType, [contextAttribute]);
-            ```
-            - contextType : A string specifying the type of context.
-                - '2d' : for 2D graphics
-                - 'webgl' : for 3D graphics
-                - 'webgl2' : for WebGL2.0
-        - .fill() method -> used to fill color in the drawn shape ; default value is black ; can be changed by setting the fillStyle property value to the favoured color
-        - .stroke method -> used to make a outline of the drawn shape ; default is 1px black outline ; can be changed by setting the .lineWidth property and .strokeStyle property values to the user accordance
-    - globalAlpha property to set the opacity of the shapes drawn
-    
+  ## How to use <canvas>
+
+  - Get the context via JS
+    ```Javascript
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d'); // 2d param for 2d graphics rendering
+    ```
+    ### getContext() method in JS
+    - used with <canvas> element to obtain a drawing context, which is an object that provides methods and properties for rendering graphics on the canvas.
+    - Syntax:
+      ```Javascript
+      canvas.getContext(contextType, [contextAttribute]);
+      ```
+      - contextType : A string specifying the type of context.
+        - '2d' : for 2D graphics
+        - 'webgl' : for 3D graphics
+        - 'webgl2' : for WebGL2.0
+    - .fill() method -> used to fill color in the drawn shape ; default value is black ; can be changed by setting the fillStyle property value to the favoured color
+    - .stroke method -> used to make a outline of the drawn shape ; default is 1px black outline ; can be changed by setting the .lineWidth property and .strokeStyle property values to the user accordance
+  - globalAlpha property to set the opacity of the shapes drawn
+
+- to limit certain canvas settings only to specific draw calls, we can wrap that drawing code between save() and restore() built-in canvas methods.
