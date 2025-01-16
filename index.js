65e4d88afa918d712485a83fab66105163683d80
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-01-16 10:37:14 +0530

    feat: add egg image and implement Egg class for game mechanics

--- index.js
+++ index.js
@@ -180,6 +180,26 @@ window.addEventListener("load", function () {
     }
   }
 
+  class Egg{
+    constructor(game){
+      this.game = game
+      this.collisionX = Math.random() * this.game.width
+      this.collisionY = Math.random() * this.game.height
+      this.collisionRadius = 40
+      this.image = document.getElementById('egg')
+      this.spriteWidth = 110
+      this.spriteHeight = 135
+      this.width = this.spriteWidth
+      this.height = this.spriteHeight
+      this.spriteX = this.collisionX + this.width * 0.5
+      this.spriteY = this.collisionY + this.height * 0.5
+    }
+
+    draw(context){
+      context
+    }
+  }
+
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
